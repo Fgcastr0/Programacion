c65eb6103200976a8962abd9b36e1b572d8d8407
Fgcastr0 <130518479+Fgcastr0@users.noreply.github.com>
2023-05-30 22:03:51 -0300

    Update Ejercicio01.java

--- Ejercicio01.java
+++ Ejercicio01.java
@@ -4,8 +4,8 @@ public class Ejercicio01 {
 
         public static void main(String[] args) {
 
-            String[] palabras = {"franco, francesca, florencia, Martina"};
-            char caracter ='f';
+            String[] palabras = {"messi, julian, lautaro, emiliano"};
+            char caracter ='m';
             int contadorPalabras = 0 ;
             for (String palabra : palabras) {
                 if (palabra.charAt(0) == caracter) {
